40302fc216b923378918612151189dada5aa736d
theo savage <theo@humanmade.co.uk>
2011-07-19 11:50:32 +0100

    Handle empty passwords in mysql_dump

--- functions/backup.mysql.functions.php
+++ functions/backup.mysql.functions.php
@@ -7,24 +7,27 @@
  * if not.
  */
 function hmbkp_backup_mysql() {
-
 	// Use mysqldump if we can
-	if ( hmbkp_mysqldump_path() )
-		
+	if ( hmbkp_mysqldump_path() ){
+
+			$cmd = escapeshellarg( hmbkp_mysqldump_path() );
+			$cmd .= ' --no-create-db ';
+			$cmd .= ' -u ' . escapeshellarg( DB_USER );
+			
+			if (DB_PASSWORD)
+				$cmd .= ' -p'  . escapeshellarg( DB_PASSWORD );
+			
+			$cmd .= ' -h ' . escapeshellarg( DB_HOST );
+			$cmd .= ' -r ' . escapeshellarg( hmbkp_path() . '/database_' . DB_NAME . '.sql' ) . ' ' . escapeshellarg( DB_NAME );
+
+
 		// Backup everything except whats in the exclude file
-		shell_exec(
-			escapeshellarg( hmbkp_mysqldump_path() )
-			. ' --no-create-db '
-			. ' -u ' . escapeshellarg( DB_USER )
-			. ' -p'  . escapeshellarg( DB_PASSWORD )
-			. ' -h ' . escapeshellarg( DB_HOST )
-			. ' -r ' . escapeshellarg( hmbkp_path() . '/database_' . DB_NAME . '.sql' ) . ' ' . escapeshellarg( DB_NAME )
-		);
-
+		shell_exec($cmd);
+}
 	// Fallback to using PHP if not
-	else
+	else{
 		hmbkp_backup_mysql_fallback();
-
+	}
 }
 
 /**
