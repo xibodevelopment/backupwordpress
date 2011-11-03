3aa934223934482cf3302c235a3274659aee7077
Tom Willmot <tom@humanmade.co.uk>
2011-11-03 18:25:09 +0000

    Bump

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@ Plugin Name: BackUpWordPress
 Plugin URI: http://humanmade.co.uk/
 Description: Simple automated backups of your WordPress powered website. Once activated you'll find me under <strong>Tools &rarr; Backups</strong>.
 Author: Human Made Limited
-Version: 1.3.3 beta
+Version: 1.4 beta
 Author URI: http://humanmade.co.uk/
 */
 
