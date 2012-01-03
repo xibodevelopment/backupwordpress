defd8173e48ed40335cbace6ea77d35e7e62aefc
Tom Willmot <tom@humanmade.co.uk>
2012-01-03 09:56:22 +0000

    More obvious that running backups can be canceled

--- admin.backup-button.php
+++ admin.backup-button.php
@@ -1,6 +1,6 @@
 <?php if ( hmbkp_is_in_progress() ) : ?>
 
-		<a id="hmbkp_backup" class="add-new-h2 hmbkp_running" href="tools.php?page=<?php echo HMBKP_PLUGIN_SLUG; ?>&amp;action=hmbkp_cancel"><?php echo hmbkp_get_status(); ?></a>
+		<a id="hmbkp_backup" class="add-new-h2 hmbkp_running" href="tools.php?page=<?php echo HMBKP_PLUGIN_SLUG; ?>&amp;action=hmbkp_cancel"><?php echo hmbkp_get_status(); ?> [cancel]</a>
 
 <?php elseif ( hmbkp_possible() ) : ?>
 
