5ca34050335eca09b81686140f9ddd23ff5878d4
Tom Willmot <tom@humanmade.co.uk>
2012-05-01 10:44:50 +0100

    Add a link to translate.hmn.md

--- admin.menus.php
+++ admin.menus.php
@@ -78,7 +78,8 @@ function hmbkp_contextual_help() {
 	get_current_screen()->set_help_sidebar(
 		'<p><strong>' . __( 'For more information:', 'hmbkp' ) . '</strong></p>' .
 		'<p><a href="https://github.com/humanmade/backupwordpress" target="_blank">github</a></p>' .
-		'<p><a href="http://wordpress.org/tags/backupwordpress?forum_id=10" target="_blank">' . __( 'Support Forums', 'hmbkp' ) .'</a></p>'
+		'<p><a href="http://wordpress.org/tags/backupwordpress?forum_id=10" target="_blank">' . __( 'Support Forums', 'hmbkp' ) .'</a></p>' .
+		'<p><a href="http://translate.hmn.md/" target="_blank">' . __( 'Help with translation', 'hmbkp' ) .'</a></p>'
 	);
 
 }
