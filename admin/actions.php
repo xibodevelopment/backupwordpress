ed9af8a97e952bc621a31f6b105b74be706a5990
Tom Willmot <tom@humanmade.co.uk>
2012-10-18 23:49:55 +0200

    Don't use strict operator to check 200 response
    Different transport methods can return `int` or `string`. Props @joehoyle

--- admin/actions.php
+++ admin/actions.php
@@ -169,7 +169,7 @@ function hmbkp_ajax_cron_test() {
 	if ( is_wp_error( $response ) )
 		echo '<div id="hmbkp-warning" class="updated fade"><p><strong>' . __( 'BackUpWordPress has detected a problem.', 'hmbkp' ) . '</strong> ' . sprintf( __( '%1$s is returning a %2$s response which could mean cron jobs aren\'t getting fired properly. BackUpWordPress relies on wp-cron to run scheduled back ups. See the %3$s for more details.', 'hmbkp' ), '<code>wp-cron.php</code>', '<code>' . $response->get_error_message() . '</code>', '<a href="http://wordpress.org/extend/plugins/backupwordpress/faq/">FAQ</a>' ) . '</p></div>';
 
-	else if ( $response['response']['code'] !== 200 )
+	else if ( $response['response']['code'] != 200 )
     	echo '<div id="hmbkp-warning" class="updated fade"><p><strong>' . __( 'BackUpWordPress has detected a problem.', 'hmbkp' ) . '</strong> ' . sprintf( __( '%1$s is returning a %2$s response which could mean cron jobs aren\'t getting fired properly. BackUpWordPress relies on wp-cron to run scheduled back ups. See the %3$s for more details.', 'hmbkp' ), '<code>wp-cron.php</code>', '<code>' . $response['response']['code'] . '</code>', '<a href="http://wordpress.org/extend/plugins/backupwordpress/faq/">FAQ</a>' ) . '</p></div>';
 
 	else
