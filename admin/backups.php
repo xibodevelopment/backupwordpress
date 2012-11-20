84b15c7453b1d24ccc77941a969e38c07ba3d818
Tom Willmot <tom@humanmade.co.uk>
2012-11-20 10:00:27 +0000

    Show the spinner next to the schedule link when a schedule is running and you are viewing a different schedule.

--- admin/backups.php
+++ admin/backups.php
@@ -6,7 +6,7 @@
 
 	<?php foreach ( $schedules->get_schedules() as $schedule ) : ?>
 
-		<li><a<?php if ( ! empty ( $_GET['hmbkp_schedule_id'] ) && $schedule->get_id() == $_GET['hmbkp_schedule_id'] ) { ?> class="current"<?php } ?> href="<?php echo esc_url( add_query_arg( 'hmbkp_schedule_id', $schedule->get_id(), HMBKP_ADMIN_URL ) ); ?> "><?php echo esc_attr( $schedule->get_name() ); ?> <span class="count">(<?php echo count( $schedule->get_backups() ); ?>)</span></a></li>
+		<li<?php if ( $schedule->get_status() ) { ?> class="hmbkp-running"<?php } ?>><a<?php if ( ! empty ( $_GET['hmbkp_schedule_id'] ) && $schedule->get_id() == $_GET['hmbkp_schedule_id'] ) { ?> class="current"<?php } ?> href="<?php echo esc_url( add_query_arg( 'hmbkp_schedule_id', $schedule->get_id(), HMBKP_ADMIN_URL ) ); ?> "><?php echo esc_attr( $schedule->get_name() ); ?> <span class="count">(<?php echo count( $schedule->get_backups() ); ?>)</span></a></li>
 
 	<?php endforeach; ?>
 
