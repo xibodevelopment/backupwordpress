438225104e68096908535355445655051a49f3e4
Tom Willmot <tom@humanmade.co.uk>
2012-11-24 17:57:20 +0000

    Fix a PHP notice on Apache servers

--- functions/core.php
+++ functions/core.php
@@ -287,10 +287,10 @@ function hmbkp_path() {
 	if ( ! file_exists( $index ) && is_writable( $path ) )
 		file_put_contents( $index, '' );
 
+	$htaccess = $path . '/.htaccess';
+
 	// Protect the directory with a .htaccess file on Apache servers
 	if ( $is_apache && function_exists( 'insert_with_markers' ) && ! file_exists( $htaccess ) && is_writable( $path ) ) {
-
-		$htaccess = $path . '/.htaccess';
 
 		$contents[]	= '# ' . sprintf( __( 'This %s file ensures that other people cannot download your backup files.', 'hmbkp' ), '.htaccess' );
 		$contents[] = '';
