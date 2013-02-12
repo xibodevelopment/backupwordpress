2c9a9d4a1831a76901f2aa595b24420a9fe6ec06
Tom Willmot <tom@humanmade.co.uk>
2013-02-12 14:59:51 +0000

    Cleanup on uninstall

---
+++ uninstall.php
@@ -0,0 +1,23 @@
+<?php
+
+if ( ! defined( 'HMBKP_PLUGIN_PATH' ) )
+	define( 'HMBKP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+// Load the schedules
+require_once( HMBKP_PLUGIN_PATH . '/hm-backup/hm-backup.php' );
+require_once( HMBKP_PLUGIN_PATH . '/classes/schedule.php' );
+require_once( HMBKP_PLUGIN_PATH . '/classes/schedules.php' );
+require_once( HMBKP_PLUGIN_PATH . '/functions/core.php' );
+
+$schedules = new HMBKP_Schedules;
+
+// Cancel all the schedules and delete all the backups
+foreach ( $schedules->get_schedules() as $schedule )
+	$schedule->cancel( true );
+
+// Remove the backups directory
+hmbkp_rmdirtree( hmbkp_path() );
+
+// Remove all the options
+foreach ( array( 'hmbkp_plugin_version', 'hmbkp_path', 'hmbkp_path_default' ) as $option )
+	delete_option( $option );
