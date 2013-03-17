111a7af63d65c62a6cf24937f3c9bf162923033f
Tom Willmot <tom@humanmade.co.uk>
2013-03-17 16:32:47 +0100

    Only try to add the FAQ if we were able to connect to the plugins API.

--- admin/menu.php
+++ admin/menu.php
@@ -72,7 +72,10 @@ function hmbkp_contextual_help() {
 	require_once( HMBKP_PLUGIN_PATH . '/admin/constants.php' );
 	$constants = ob_get_clean();
 
-	get_current_screen()->add_help_tab( array( 'title' => __( 'FAQ', 'hmbkp' ), 'id' => 'hmbkp_faq', 'content' => wp_kses_post( $warning . $plugin->sections['faq'] ) ) );
+
+	if ( $plugin && ! is_wp_error( $plugin ) )
+		get_current_screen()->add_help_tab( array( 'title' => __( 'FAQ', 'hmbkp' ), 'id' => 'hmbkp_faq', 'content' => wp_kses_post( $warning . $plugin->sections['faq'] ) ) );
+	
 	get_current_screen()->add_help_tab( array( 'title' => __( 'Constants', 'hmbkp' ), 'id' => 'hmbkp_constants', 'content' => wp_kses_post( $constants ) ) );
 
 	get_current_screen()->set_help_sidebar(
