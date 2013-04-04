c729622445898e2987697b1c27ded31da4dc94c6
Paul de Wouters <paul@desktop>
2013-04-04 10:33:19 -0300

    dont hardcode slug

--- plugin.php
+++ plugin.php
@@ -27,7 +27,7 @@ Author URI: http://hmn.md/
 */
 
 if ( ! defined( 'HMBKP_PLUGIN_SLUG' ) )
-	define( 'HMBKP_PLUGIN_SLUG', 'backupwordpress' );
+	define( 'HMBKP_PLUGIN_SLUG', basename( dirname( __FILE__ ) ) );
 
 if ( ! defined( 'HMBKP_PLUGIN_PATH' ) )
 	define( 'HMBKP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
