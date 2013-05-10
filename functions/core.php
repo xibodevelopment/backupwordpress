b7437b17dbeb990163c61f0a7d42df8561bd2ecf
Paul de Wouters <paul@desktop>
2013-05-10 10:42:21 -0300

    use constants in schedul intervals

--- functions/core.php
+++ functions/core.php
@@ -201,12 +201,12 @@ add_action( 'admin_init', 'hmbkp_setup_default_schedules' );
 function hmbkp_cron_schedules() {
 
 	return array(
-		'hourly'     	=> array( 'interval' => 3600, 		 'display' => __( 'Once Hourly' ) ),
-		'twicedaily' 	=> array( 'interval' => 43200,		 'display' => __( 'Twice Daily' ) ),
-		'daily'      	=> array( 'interval' => 86400,		 'display' => __( 'Once Daily' ) ),
-	    'weekly' 		=> array( 'interval' => 604800,		 'display' => __( 'Once Weekly', 'hmbkp' ) ),
-	    'fortnightly'	=> array( 'interval' => 1209600,	 'display' => __( 'Once Fortnightly', 'hmbkp' ) ),
-	    'monthly'		=> array( 'interval' => 2629743.83,  'display' => __( 'Once Monthly', 'hmbkp' ) )
+		'hourly'     	=> array( 'interval' => HOUR_IN_SECONDS, 'display'      => __( 'Once Hourly', 'hmbkp' ) ),
+		'twicedaily' 	=> array( 'interval' => 12 * HOUR_IN_SECONDS, 'display' => __( 'Twice Daily', 'hmbkp' ) ),
+		'daily'      	=> array( 'interval' => DAY_IN_SECONDS, 'display'       => __( 'Once Daily', 'hmbkp' ) ),
+	  'weekly' 		  => array( 'interval' => WEEK_IN_SECONDS, 'display'      => __( 'Once Weekly', 'hmbkp' ) ),
+	  'fortnightly'	=> array( 'interval' => 2 * WEEK_IN_SECONDS , 'display' => __( 'Once Fortnightly', 'hmbkp' ) ),
+	  'monthly'		  => array( 'interval' => 30 * DAY_IN_SECONDS, 'display'  => __( 'Once Monthly', 'hmbkp' ) )
 	);
 
 }
