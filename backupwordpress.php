38aa2edc6300087d804de83f6b386ed2c7437ec7
Tom Willmot <tom@humanmade.co.uk>
2013-06-25 13:31:23 +0100

    Don't indent the copyright notice

--- backupwordpress.php
+++ backupwordpress.php
@@ -9,21 +9,22 @@ Version: 2.3 beta
 Author URI: http://hmn.md/
 */
 
-/*  Copyright 2013 Human Made Limited  (email : support@hmn.md)
+/*
+Copyright 2011 - 2013 Human Made Limited  (email : support@hmn.md)
 
-		This program is free software; you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation; either version 2 of the License, or
-		(at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-		You should have received a copy of the GNU General Public License
-		along with this program; if not, write to the Free Software
-		Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 if ( ! defined( 'HMBKP_PLUGIN_SLUG' ) )
