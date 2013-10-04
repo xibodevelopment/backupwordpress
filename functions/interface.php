af22b2d558411461679fd24d87801759c9b70944
Paul de Wouters <paul@hmn.md>
2013-10-04 14:58:39 +0200

    check if path is accessible before displaying download link

--- functions/interface.php
+++ functions/interface.php
@@ -25,9 +25,11 @@ function hmbkp_get_backup_row( $file, HMBKP_Scheduled_Backup $schedule ) {
 
 		<td>
 
+			<?php if (  hmbkp_is_path_accessible( hmbkp_path() )  ) : ?>
 			<a href="<?php echo wp_nonce_url( admin_url( 'tools.php?page=' . HMBKP_PLUGIN_SLUG . '&amp;hmbkp_download_backup=' . $encoded_file . '&amp;hmbkp_schedule_id=' . $schedule->get_id() ), 'hmbkp-download_backup' ); ?>"><?php _e( 'Download', 'hmbkp' ); ?></a> |
 			<a href="<?php echo wp_nonce_url( admin_url( 'tools.php?page=' . HMBKP_PLUGIN_SLUG . '&amp;hmbkp_delete_backup=' . $encoded_file . '&amp;hmbkp_schedule_id=' . $schedule->get_id() ), 'hmbkp-delete_backup' ); ?>" class="delete-action"><?php _e( 'Delete', 'hmbkp' ); ?></a>
 
+			<?php endif; ?>
 		</td>
 
 	</tr>
