58eb3c50156c2171a19d5b38296d372c0eb74497
Paul de Wouters <paul@hmn.md>
2013-10-06 16:13:45 +0200

    remove some unnecessary code

--- backupwordpress.php
+++ backupwordpress.php
@@ -196,8 +196,6 @@ function load_intercom_script() {
 
 <?php }
 
-$hmbkp_intercom_opt_in = get_option( 'hmbkp_intercom_opt_in' );
-
 if ( false != ( $hmbkp_intercom_opt_in = get_option( 'hmbkp_intercom_opt_in' ) ) )
 	add_action( 'admin_footer-tools_page_backupwordpress', 'load_intercom_script' );
 
@@ -253,44 +251,3 @@ function hmbkp_plugin_textdomain() {
 
 }
 add_action( 'init', 'hmbkp_plugin_textdomain', 1 );
-
-
-add_action( 'contextual_help', 'wptuts_screen_help', 10, 3 );
-function wptuts_screen_help( $contextual_help, $screen_id, $screen ) {
-	// The add_help_tab function for screen was introduced in WordPress 3.3.
-	if ( ! method_exists( $screen, 'add_help_tab' ) )
-		return $contextual_help;
-	global $hook_suffix;
-	// List screen properties
-	$variables = '<ul style="width:50%;float:left;"> <strong>Screen variables </strong>'
-			. sprintf( '<li> Screen id : %s</li>', $screen_id )
-			. sprintf( '<li> Screen base : %s</li>', $screen->base )
-			. sprintf( '<li>Parent base : %s</li>', $screen->parent_base )
-			. sprintf( '<li> Parent file : %s</li>', $screen->parent_file )
-			. sprintf( '<li> Hook suffix : %s</li>', $hook_suffix )
-			. '</ul>';
-	// Append global $hook_suffix to the hook stems
-	$hooks = array(
-		"load-$hook_suffix",
-		"admin_print_styles-$hook_suffix",
-		"admin_print_scripts-$hook_suffix",
-		"admin_head-$hook_suffix",
-		"admin_footer-$hook_suffix"
-	);
-	// If add_meta_boxes or add_meta_boxes_{screen_id} is used, list these too
-	if ( did_action( 'add_meta_boxes_' . $screen_id ) )
-		$hooks[] = 'add_meta_boxes_' . $screen_id;
-	if ( did_action( 'add_meta_boxes' ) )
-		$hooks[] = 'add_meta_boxes';
-	// Get List HTML for the hooks
-	$hooks = '<ul style="width:50%;float:left;"> <strong>Hooks </strong> <li>' . implode( '</li><li>', $hooks ) . '</li></ul>';
-	// Combine $variables list with $hooks list.
-	$help_content = $variables . $hooks;
-	// Add help panel
-	$screen->add_help_tab( array(
-		'id'      => 'wptuts-screen-help',
-		'title'   => 'Screen Information',
-		'content' => $help_content,
-	));
-	return $contextual_help;
-}
