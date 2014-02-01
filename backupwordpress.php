d632660aceab9fd9ec6e789d232d708739ee0973
Tom Willmot <willmot@Thomas-Willmots-MacBook-Air.local>
2014-02-01 17:56:04 +0100

    Remove back-compat time constants
    These were added to core in 3.5, since we bumped the minimum requirements to 3.7.1 we can get rid of our back-compat versions.

--- backupwordpress.php
+++ backupwordpress.php
@@ -69,21 +69,6 @@ if ( ! defined( 'HMBKP_SCHEDULE_TIME' ) )
 if ( ! defined( 'HMBKP_REQUIRED_PHP_VERSION' ) )
 	define( 'HMBKP_REQUIRED_PHP_VERSION', '5.2.4' );
 
-if ( ! defined( 'MINUTE_IN_SECONDS' ) )
-	define( 'MINUTE_IN_SECONDS', 60 );
-
-if ( ! defined( 'HOUR_IN_SECONDS' ) )
-	define( 'HOUR_IN_SECONDS',   60 * MINUTE_IN_SECONDS );
-
-if ( ! defined( 'DAY_IN_SECONDS' ) )
-	define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
-
-if ( ! defined( 'WEEK_IN_SECONDS' ) )
-	define( 'WEEK_IN_SECONDS',    7 * DAY_IN_SECONDS    );
-
-if ( ! defined( 'YEAR_IN_SECONDS' ) )
-	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
-
 if ( ! defined( 'HMBKP_ADMIN_PAGE' ) ) {
 
 	if ( is_multisite() )
