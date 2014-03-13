cc70d0b04053945eaae9ab9575b5949da43d590e
Paul de Wouters <paul@hmn.md>
2014-03-13 15:04:24 -0300

    Use core copy_dir function and some refactoring
    Signed-off-by: Paul de Wouters <paul@hmn.md>

--- classes/class-hmbkp-files-restore.php
+++ classes/class-hmbkp-files-restore.php
@@ -4,66 +4,44 @@ class HMBKP_Files_Restore {
 
 	protected $root_path;
 
-	public function __construct( $root_path, $dump_file_archive ) {
+	protected $restore_from_path;
 
-		$this->root_path = $root_path;
+	public function __construct( $restore_from_path ) {
 
-	}
+		$this->root_path = get_home_path();
 
-	public function backup_install() {
-
-		$backup_path = $this->root_path . '/bwp_' . time();
-
-		if ( ! is_dir( $backup_path ) ) {
-			mkdir( $backup_path );
-		}
-
-		foreach (
-			$iterator = new RecursiveIteratorIterator(
-				new RecursiveDirectoryIterator($this->root_path, RecursiveDirectoryIterator::SKIP_DOTS ),
-				RecursiveIteratorIterator::SELF_FIRST, RecursiveIteratorIterator::CATCH_GET_CHILD ) as $item
-		) {
-
-			if( $item->isDir() && ( strpos( $item->getPath(), $backup_path ) !== false ) )
-				continue;
-			if( is_link( $item ) ) {
-				symlink( readlink( $item ), $backup_path . DIRECTORY_SEPARATOR . $iterator->getSubPathName() );
-			} elseif ( $item->isDir() ) {
-				mkdir( $backup_path . DIRECTORY_SEPARATOR . $iterator->getSubPathName() );
-			} else {
-				copy( $item->getRealPath(), $backup_path . DIRECTORY_SEPARATOR . $iterator->getSubPathName() . $item->getFilename );
-			}
-		}
-
-	}
-
-	public function copy_dir($src, $dst)
-	{
-
-		if (is_link($src)) {
-			symlink(readlink($src), $dst);
-		} elseif (is_dir($src)) {
-			mkdir($dst);
-			foreach (scandir($src) as $file) {
-				if ($file != '.' && $file != '..') {
-					if( $dst !== $src )
-						continue;
-					copy_dir("$src/$file", "$dst/$file");
-				}
-			}
-		} elseif (is_file($src)) {
-			copy($src, $dst);
-		} else {
-			echo "WARNING: Cannot copy $src (unknown file type)\n";
-		}
+		$this->restore_from_path = $restore_from_path;
 	}
 
 	public function restore_files() {
 
-		$backup_path = $this->root_path . '/bwp_' . time();
+		$this->copy_old_files();
 
-		//$this->backup_install();
-		$this->copy_dir( $this->root_path, $backup_path );
+		$this->copy_new_files();
+
+	}
+
+	public function copy_old_files() {
+
+		$backup_folder =  'bwp_' . time();
+		$backup_path = $this->root_path . $backup_folder;
+
+		if ( ! is_dir( $backup_path ) )
+			if ( ! mkdir( $backup_path ) )
+				return;
+
+		// TODO: alternative method to copy if copy_dir not available
+		WP_Filesystem();
+
+		copy_dir( $this->root_path, $backup_path, array( $backup_folder, '.git' ) );
+
+	}
+
+	public function copy_new_files() {
+
+		WP_Filesystem();
+
+		copy_dir( $this->restore_from_path, $this->root_path );
 
 	}
 
