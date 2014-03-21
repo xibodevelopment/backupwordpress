15a2e57aab1c49a5f3fd0732e0b7e3735143540a
Tom Willmot <willmot@thomaswlmotsair.wifi.eastmidlandstrains.co.uk>
2014-03-21 12:04:53 +0000

    Correctly highlight the tab for the first schedule

--- admin/backups.php
+++ admin/backups.php
@@ -1,9 +1,15 @@
-<?php $schedules = HMBKP_Schedules::get_instance()->get_schedules(); ?>
+<?php $schedules = HMBKP_Schedules::get_instance()->get_schedules();
+
+if ( ! empty( $_GET['hmbkp_schedule_id'] ) )
+	$current_schedule = new HMBKP_Scheduled_Backup( sanitize_text_field( $_GET['hmbkp_schedule_id'] ) );
+else {
+	$current_schedule = reset( $schedules );
+} ?>
 
 <h2 class="nav-tab-wrapper">
 
 	<?php foreach ( $schedules as $schedule ) : ?>
-		<a class="nav-tab<?php if ( $schedule->get_status() ) { ?> hmbkp-running<?php } ?><?php if ( ! empty ( $_GET['hmbkp_schedule_id'] ) && $schedule->get_id() === $_GET['hmbkp_schedule_id'] ) { ?> nav-tab-active<?php } ?>" <?php if ( $schedule->get_status() ) { ?>title="<?php echo esc_attr( strip_tags( $schedule->get_status() ) ); ?>"<?php } ?> href="<?php echo esc_url( add_query_arg( 'hmbkp_schedule_id', $schedule->get_id(), HMBKP_ADMIN_URL ) ); ?> "><?php echo esc_html( hmbkp_translated_schedule_title( $schedule->get_slug(), $schedule->get_name() ) ); ?> <span class="count">(<?php echo esc_html( count( $schedule->get_backups() ) ); ?>)</span></a>
+		<a class="nav-tab<?php if ( $schedule->get_status() ) { ?> hmbkp-running<?php } ?><?php if ( ! empty ( $current_schedule ) && $schedule->get_id() === $current_schedule->get_id() ) { ?> nav-tab-active<?php } ?>" <?php if ( $schedule->get_status() ) { ?>title="<?php echo esc_attr( strip_tags( $schedule->get_status() ) ); ?>"<?php } ?> href="<?php echo esc_url( add_query_arg( 'hmbkp_schedule_id', $schedule->get_id(), HMBKP_ADMIN_URL ) ); ?> "><?php echo esc_html( hmbkp_translated_schedule_title( $schedule->get_slug(), $schedule->get_name() ) ); ?> <span class="count">(<?php echo esc_html( count( $schedule->get_backups() ) ); ?>)</span></a>
 	<?php endforeach; ?>
 
 	<a class="nav-tab" href="<?php esc_attr_e( esc_url( add_query_arg( array( 'action' => 'hmbkp_add_schedule_load' ), is_multisite() ? network_admin_url( 'admin-ajax.php' ) : admin_url( 'admin-ajax.php' ) ) ) ); ?>"> + <?php _e( 'add schedule', 'hmbkp' ); ?></a>
@@ -16,13 +22,7 @@
 
 </h2>
 
-<?php if ( ! empty( $_GET['hmbkp_schedule_id'] ) )
-	$schedule = new HMBKP_Scheduled_Backup( sanitize_text_field( $_GET['hmbkp_schedule_id'] ) );
-else {
-	$schedule = reset( $schedules );
-}
-
-// Don't continue if we don't have a schedule
+<?php // Don't continue if we don't have a schedule
 if ( ! $schedule )
 	return; ?>
 
