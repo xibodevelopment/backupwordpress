f39a2c7a3db73d489ef613f2f5c5be5fbbfbdad4
Paul de Wouters <paul@hmn.md>
2014-07-28 14:05:08 +0200

    Remove sprintf
    sprintf makes it impossible to use regexes to generate the readme.

--- src/admin/faq.php
+++ src/admin/faq.php
@@ -1,19 +1,19 @@
 <?php
 echo '<p><strong>' . __( 'Where does BackUpWordPress store the backup files?', 'hmbkp' ) . '</strong></p>' .
 
-'<p>' . sprintf( __( 'Backups are stored on your server in %s, you can change the directory.', 'hmbkp' ), '<code>/wp-content/backups</code>' ) . '</p>' .
+'<p>' . __( 'Backups are stored on your server in <code>/wp-content/backups</code>, you can change the directory.', 'hmbkp' ). '</p>' .
 
 '<p>' . __( 'Important: By default BackUpWordPress backs up everything in your site root as well as your database, this includes any non WordPress folders that happen to be in your site root. This does mean that your backup directory can get quite large.', 'hmbkp' ) . '</p>' .
 
 '<p><strong>' . __( 'What if I want to back up my site to another destination?', 'hmbkp' ) . '</strong></p>' .
 
-'<p>' . sprintf( __( 'BackUpWordPress Pro supports Dropbox, Google Drive, Amazon S3, Rackspace, Azure, DreamObjects and FTP/SFTP. Check it out here: %s', 'hmbkp' ), '<a href="http://bwp.hmn.md/?utm_source=wordpress-org&utm_medium=plugin-page&utm_campaign=freeplugin" target="_blank" title="BackUpWordPress Homepage">https://bwp.hmn.md</a>' ) . '</p>' .
+'<p>' . __( 'BackUpWordPress Pro supports Dropbox, Google Drive, Amazon S3, Rackspace, Azure, DreamObjects and FTP/SFTP. Check it out here: <a href="http://bwp.hmn.md/?utm_source=wordpress-org&utm_medium=plugin-page&utm_campaign=freeplugin" target="_blank" title="BackUpWordPress Homepage">https://bwp.hmn.md</a>', 'hmbkp' ) . '</p>' .
 
 '<p><strong>' . __( 'How do I restore my site from a backup?', 'hmbkp' ) . '</strong></p>' .
 
-'<p>' . sprintf( __( 'You need to download the latest backup file either by clicking download on the backups page or via %s. %s the files and upload all the files to your server overwriting your site. You can then import the database using your hosts database management tool (likely %s).', 'hmbkp' ), '<code>FTP</code>', '<code>Unzip</code>', '<code>phpMyAdmin</code>' ) . '</p>' .
+'<p>' . __( 'You need to download the latest backup file either by clicking download on the backups page or via <code>FTP</code>. <code>Unzip</code> the files and upload all the files to your server overwriting your site. You can then import the database using your hosts database management tool (likely <code>phpMyAdmin</code>).', 'hmbkp' ) . '</p>' .
 
-'<p>' . sprintf( __( 'See this guide for more details - %s.', 'hmbkp' ), '<a href="https://bwp.hmn.md/support-center/restore-backup/" target="_blank">How to restore from backup</a>' ) . '</p>' .
+'<p>' . __( 'See this guide for more details - <a href="https://bwp.hmn.md/support-center/restore-backup/" target="_blank">How to restore from backup</a>.', 'hmbkp' ) . '</p>' .
 
 '<p><strong>' . __( 'Does BackUpWordPress back up the backups directory?', 'hmbkp' ) . '</strong></p>' .
 
@@ -33,21 +33,25 @@ echo '<p><strong>' . __( 'Where does BackUpWordPress store the backup files?', '
 
 '<p><strong>' . __( 'What do I do if I get the wp-cron error message?', 'hmbkp' ) . '</strong></p>' .
 
-'<p>' . sprintf( __( 'The issue is that your %s is not returning a %s response when hit with a HTTP request originating from your own server, it could be several things, in most cases, it\'s an issue with the server / site.', 'hmbkp' ), '<code>wp-cron.php</code>', '<code>200</code>' ) . '</p>' .
+'<p>' . __( 'The issue is that your <code>wp-cron.php</code> is not returning a <code>200</code> response when hit with a HTTP request originating from your own server, it could be several things, in most cases, it\'s an issue with the server / site.', 'hmbkp' ) . '</p>' .
 
 '<p>' . __( 'There are some things you can test to confirm this is the issue.', 'hmbkp' ) . '</p>' .
 
 '<ul><li>' . __( 'Are scheduled posts working? (They use wp-cron as well ). ', 'hmbkp' ) . '</li>' .
+
 '<li>' . __( 'Are you hosted on Heart Internet? (wp-cron may not be supported by Heart Internet, see below for work-around).', 'hmbkp' ) . '</li>' .
+
 '<li>' . __( 'If you click manual backup does it work?', 'hmbkp' ) . '</li>' .
-'<li>' . sprintf( __( 'Try adding %s to your %s, do automatic backups work?', 'hmbkp' ), '<code>define( \'ALTERNATE_WP_CRON\', true );</code>', '<code>wp-config.php</code>' ) . '</li>' .
+
+'<li>' . __( 'Try adding <code>define( \'ALTERNATE_WP_CRON\', true );</code> to your <code>wp-config.php</code>, do automatic backups work?', 'hmbkp' ) . '</li>' .
+
 '<li>' . __( 'Is your site private (I.E. is it behind some kind of authentication, maintenance plugin, .htaccess) if so wp-cron won\'t work until you remove it, if you are and you temporarily remove the authentication, do backups start working?', 'hmbkp' ) . '</li></ul>' .
 
 '<p>' . __( 'Report the results to our support team for further help. To do this, either enable suport from your Admin Dashboard (recommended), or email support@hmn.md', 'hmbkp' ) . '</p>' .
 
 '<p><strong>' . __( 'How to get BackUpWordPress working in Heart Internet', 'hmbkp' ) . '</strong></p>' .
 
-'<p>' . sprintf( __( 'The script to be entered into the Heart Internet cPanel is: %s (note the space between php5 and the location of the file). The file %s %s must be set to %s.', 'hmbkp' ), '<code>/usr/bin/php5 /home/sites/yourdomain.com/public_html/wp-cron.php</code>', '<code>wp-cron.php</code>', '<code>chmod</code>', '<code>711</code>' ) . '</p>' .
+'<p>' . __( 'The script to be entered into the Heart Internet cPanel is: <code>/usr/bin/php5 /home/sites/yourdomain.com/public_html/wp-cron.php</code> (note the space between php5 and the location of the file). The file <code>wp-cron.php</code> <code>chmod</code> must be set to <code>711</code>.', 'hmbkp' ) . '</p>' .
 
 '<p><strong>' . __( 'My backups seem to be failing?', 'hmbkp' ) . '</strong></p>' .
 
