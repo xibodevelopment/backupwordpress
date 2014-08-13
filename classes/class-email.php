dc33eac427322da8fd5f5c6acb0dd5588d584a70
Paul de Wouters <paul@hmn.md>
2014-08-13 11:44:26 -0300

    Fix property name

--- classes/class-email.php
+++ classes/class-email.php
@@ -17,7 +17,7 @@ class HMBKP_Email_Service extends HMBKP_Service {
 	 * Determines whether to show or hide the service tab in destinations form
 	 * @var boolean
 	 */
-	public $isTabVisible = false;
+	public $is_tab_visible = false;
 
 	/**
 	 * Output the email form field
