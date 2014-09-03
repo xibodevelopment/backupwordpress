b13606206701d00eeffde199e38f2b27dc182b12
Paul de Wouters <paul@hmn.md>
2014-09-03 18:36:04 -0300

    Add a class to track common errors and their nice message

--- classes/error/class-errors.php
+++ classes/error/class-errors.php
@@ -1,17 +1,49 @@
 <?php
 
+/**
+ * Class HMBKP_Known_Errors
+ */
 class HMBKP_Known_Errors {
 
+	/**
+	 * The single instance of this class.
+	 *
+	 * @var HMBKP_Known_Errors
+	 */
 	private static $_instance;
 
+	/**
+	 * Stores all possible error messages.
+	 * possible_error[error_context][error_category][message]
+	 * @var array
+	 */
 	protected $possible_errors = array(
-		'insufficient_disk_space' => array(
-			'zip I/O error: Disk quota exceeded',
-			'zip error: Output file write failure',
-			'PCL_ZIP_ERR_MISSING_FILE'
+		'backup_errors' => array(
+			'insufficient_disk_space' => array(
+				'zip I/O error: Disk quota exceeded',
+				'zip error: Output file write failure',
+				'PCL_ZIP_ERR_MISSING_FILE'
+			),
 		),
 	);
 
+	protected $nice_error_messages = array();
+
+	/**
+	 * Constructor should not be publicly accessible.
+	 */
+	private function __construct() {
+
+		$this->nice_error_messages = array(
+			'insufficient_disk_space' => __( 'Your backups folder location has insufficient free disk space' , 'hmbkp' ),
+		);
+	}
+
+	/**
+	 * Provides access to the singleton instance.
+	 *
+	 * @return HMBKP_Known_Errors
+	 */
 	public static function get_instance() {
 
 		if ( ! ( self::$_instance instanceof HMBKP_Known_Errors ) ) {
@@ -20,11 +52,18 @@ class HMBKP_Known_Errors {
 		}
 	}
 
-	public function get_all_possible_errors() {
-		return $this->possible_errors;
-	}
+	/**
+	 * Fetch possible error messages.
+	 *
+	 * @param string $type
+	 *
+	 * @return array
+	 */
+	public function get_possible_errors( $type = 'all' ) {
 
-	public function get_possible_errors_of_type( $type ) {
+		if ( 'all' === $type ) {
+			return $this->possible_errors;
+		}
 
 		return $this->possible_errors[ $type ];
 	}
@@ -32,25 +71,32 @@ class HMBKP_Known_Errors {
 	/**
 	 * Return the matching error message.
 	 *
-	 * @param $message
+	 * @param string $message
 	 */
-	public function match( $message ) {
+	public function match( $error_message ) {
 
-		foreach ( $this->possible_errors as $category -> $errors ) {
-			if ( $match = $this->array_find( $errors, $message ) ) {
-				return $match;
-			} else {
-				return $message;
+		foreach ( $this->possible_errors as $category => $types ) {
+			foreach( $types as $key => $error_type ) {
+				if ( $match = $this->array_find( $error_type, $error_message ) ) {
+					return $this->get_nice_error_message( $key );
+				} else {
+					return sprintf( __( 'Unhandled error: %s', 'hmbkp' ), $error_message );
+				}
 			}
+
 		}
 
+	}
+
+	protected function get_nice_error_message( $key ) {
+		return $this->nice_error_messages[ $key ];
 	}
 
 	/**
 	 * Find a partial match.
 	 *
 	 * @param array $haystack
-	 * @param       $needle
+	 * @param string $needle
 	 *
 	 * @return bool|int|string
 	 */
