b3926bf74e8b4b422e6cf578ad02008aa8a4a638
paul de wouters <paul@hmn.md>
2014-09-17 17:59:36 -0300

    Remove unneeded code

--- functions/interface.php
+++ functions/interface.php
@@ -11,14 +11,6 @@ function hmbkp_get_backup_row( $file, HMBKP_Scheduled_Backup $schedule ) {
 	$encoded_file = urlencode( base64_encode( $file ) );
 	$offset       = get_option( 'gmt_offset' ) * 3600;
 
-	if ( is_multisite() ) {
-		$bookmark = add_query_arg( array( 'hmbkp_schedule_id' => $schedule->get_id() ), network_admin_url( 'settings.php?page=' . HMBKP_PLUGIN_SLUG ) );
-		$download_action_url = network_admin_url( 'settings.php?page=' . HMBKP_PLUGIN_SLUG . '&amp;hmbkp_download_backup=' . $encoded_file . '&amp;hmbkp_schedule_id=' . $schedule->get_id() );
-	} else {
-		$bookmark = add_query_arg( array( 'hmbkp_schedule_id' => $schedule->get_id() ), admin_url( 'tools.php?page=' . HMBKP_PLUGIN_SLUG ) );
-		//$delete_action_url =  admin_url( 'tools.php?page=' . HMBKP_PLUGIN_SLUG . '&amp;hmbkp_delete_backup=' . $encoded_file . '&amp;hmbkp_schedule_id=' . $schedule->get_id() );
-		$download_action_url =  admin_url( 'tools.php?page=' . HMBKP_PLUGIN_SLUG . '&amp;hmbkp_download_backup=' . $encoded_file . '&amp;hmbkp_schedule_id=' . $schedule->get_id() );
-	}
 	?>
 
 	<tr class="hmbkp_manage_backups_row">
