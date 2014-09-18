cd5ca414928a9ec867fbea922f4ae758e25e2b3d
paul de wouters <paul@hmn.md>
2014-09-18 09:51:48 -0300

    Rename nonce and action

--- admin/schedule-form.php
+++ admin/schedule-form.php
@@ -14,12 +14,12 @@
 
 <?php } ?>
 
-<form method="post" class="hmbkp-form" novalidate data-schedule-action="<?php if ( isset( $is_new_schedule ) ) { ?>add<?php } else { ?>edit<?php } ?>">
+<form method="post" action="<?php echo admin_url( 'admin-post.php' ); ?>">
 
 	<input type="hidden" name="hmbkp_schedule_id" value="<?php echo esc_attr( $schedule->get_id() ); ?>" />
-	<input type="hidden" name="action" value="hmbkp_edit_schedule" />
+	<input type="hidden" name="action" value="hmbkp_edit_schedule_submit" />
 
-	<?php wp_nonce_field( 'hmbkp-edit_schedule' ); ?>
+	<?php wp_nonce_field( 'hmbkp-edit-schedule', 'hmbkp-edit-schedule-nonce' ); ?>
 
 	<table class="form-table">
 
