b18075de358aacf6f72bd71cd7623fbf8764b977
paul de wouters <paul@hmn.md>
2014-10-20 10:08:09 -0300

    WordPress Coding Standards

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,12 @@
 <?php
 
-if ( ! current_user_can( 'activate_plugins' ) )
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	return;
+}
 
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit;
+if ( ! current_user_can( 'activate_plugins' ) ) {
+	return;
+}
 
 if ( ! defined( 'HMBKP_REQUIRED_WP_VERSION' ) ) {
 	define( 'HMBKP_REQUIRED_WP_VERSION', '3.8.4' );
@@ -13,11 +15,13 @@ if ( ! defined( 'HMBKP_REQUIRED_WP_VERSION' ) ) {
 // Don't activate on old versions of WordPress
 global $wp_version;
 
-if ( version_compare( $wp_version, HMBKP_REQUIRED_WP_VERSION, '<' ) )
+if ( version_compare( $wp_version, HMBKP_REQUIRED_WP_VERSION, '<' ) ) {
 	return;
+}
 
-if ( ! defined( 'HMBKP_PLUGIN_PATH' ) )
+if ( ! defined( 'HMBKP_PLUGIN_PATH' ) ) {
 	define( 'HMBKP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+}
 
 // Load the schedules
 require_once( HMBKP_PLUGIN_PATH . 'hm-backup/hm-backup.php' );
@@ -42,6 +46,6 @@ foreach ( array( 'hmbkp_enable_support', 'hmbkp_plugin_version', 'hmbkp_path', '
 }
 
 // Delete all transients
-foreach( array( 'hmbkp_plugin_data', 'hmbkp_directory_filesizes', 'hmbkp_directory_filesize_running' ) as $transient ) {
+foreach ( array( 'hmbkp_plugin_data', 'hmbkp_directory_filesizes', 'hmbkp_directory_filesize_running' ) as $transient ) {
 	delete_transient( $transient );
 }
