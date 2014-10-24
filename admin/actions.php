ab6d05846888160680c5ae8e2e3369ef6f27f42c
paul de wouters <paul@hmn.md>
2014-10-24 11:47:33 -0300

    Use correct action hook

--- admin/actions.php
+++ admin/actions.php
@@ -20,7 +20,7 @@ function hmbkp_request_delete_backup() {
 	die;
 
 }
-add_action( 'load-' . HMBKP_ADMIN_PAGE, 'hmbkp_request_delete_backup' );
+add_action( 'admin_post_hmbkp_request_delete_backup', 'hmbkp_request_delete_backup' );
 
 /**
  * Enable support and then redirect back to the backups page
