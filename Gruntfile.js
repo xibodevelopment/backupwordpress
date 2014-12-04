a490f775ebbf947f73ab74a357c52d72f55072d5
paul de wouters <paul@hmn.md>
2014-12-04 17:21:01 -0300

    Add Grunt task options

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,34 @@ module.exports = function (grunt) {
 			}
 		},
 
+        checktextdomain: {
+            standard{
+                options:{
+                    text_domain: 'backupwordpress', //Specify allowed domain(s)
+                    keywords: [ //List keyword specifications
+                        '__:1,2d',
+                        '_e:1,2d',
+                        '_x:1,2c,3d',
+                        'esc_html__:1,2d',
+                        'esc_html_e:1,2d',
+                        'esc_html_x:1,2c,3d',
+                        'esc_attr__:1,2d',
+                        'esc_attr_e:1,2d',
+                        'esc_attr_x:1,2c,3d',
+                        '_ex:1,2c,3d',
+                        '_n:1,2,4d',
+                        '_nx:1,2,4c,5d',
+                        '_n_noop:1,2,3d',
+                        '_nx_noop:1,2,3c,4d'
+                    ]
+                },
+                files: [{
+                    src: ['**/*.php'], //all php
+                    expand: true,
+                }],
+            }
+        },
+
 		wp_readme_to_markdown: {
 			target: {
 				files: {
