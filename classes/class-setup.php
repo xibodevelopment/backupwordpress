0e6febe54a13b32d7586fb807b620d32c8d7ad2a
paul de wouters <paul@hmn.md>
2014-12-21 14:23:39 -0300

    Introduce a setup class
    This class handles the activation and deactiavtion of the plugin

---
+++ classes/class-setup.php
@@ -0,0 +1,179 @@
+<?php
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Class BackUpWordPress_Setup
+ */
+class BackUpWordPress_Setup {
+
+	/**
+	 * Defines the minimum version of WordPress required by BWP.
+	 */
+	const BWP_MIN_WP_VERSION = '3.9.3';
+
+	/**
+	 * Defines the minimum version of PHP required by BWP.
+	 */
+	const BWP_MIN_PHP_VERSION = '5.3.2';
+
+	/**
+	 * Setup the plugin defaults on activation
+	 */
+	public static function activate() {
+
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+
+			wp_die( self::get_notice_message(), __( 'BackUpWordPress', 'backupwordpress' ), array( 'back_link' => true ) );
+
+
+		// loads the translation files
+		load_plugin_textdomain( 'backupwordpress', false, HMBKP_PLUGIN_LANG_DIR );
+
+		// Run deactivate on activation in-case it was deactivated manually
+		self::deactivate();
+
+	}
+
+	/**
+	 * Cleanup on plugin deactivation
+	 *
+	 * Removes options and clears all cron schedules
+	 */
+	public static function deactivate() {
+
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+
+		// Determine if we need to do any cleanup
+		if ( ! class_exists( 'HMBKP_Schedules' ) ) {
+			return;
+		}
+
+		$schedules = HMBKP_Schedules::get_instance();
+
+		if ( empty( $schedules ) ) {
+			return;
+		}
+
+		// Clean up the backups directory
+		hmbkp_cleanup();
+
+		// Clear schedule crons
+		foreach ( $schedules->get_schedules() as $schedule ) {
+			$schedule->unschedule();
+		}
+
+		// Opt them out of support
+		delete_option( 'hmbkp_enable_support' );
+
+		// Remove the directory filesize cache
+		delete_transient( 'hmbkp_directory_filesizes' );
+
+	}
+
+	/**
+	 * Determine if environment meets all requirements for BWP to run or deactivate.
+	 */
+	public static function maybe_self_deactivate() {
+
+		// Check that this WordPress install can run BackUpWordPress.
+		if ( ! self::meets_requirements() ) {
+
+			add_action( 'admin_init', array( 'BackUpWordPress_Setup', 'self_deactivate' ) );
+
+			add_action( 'admin_notices', array( 'BackUpWordPress_Setup', 'display_admin_notices' ) );
+
+			return true;
+
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * Deactivate BackUpWordPress.
+	 */
+	public static function self_deactivate() {
+
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+
+		if ( ! function_exists( 'deactivate_plugins' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		}
+
+		deactivate_plugins( dirname( dirname( __FILE__  ) ) . '/backupwordpress.php' );
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+	}
+
+	/**
+	 * Determine if this WordPress install meets the minimum requirements for BWP to run.
+	 *
+	 * @return bool
+	 */
+	public static function meets_requirements() {
+
+		if ( ! self::is_supported_php_version() || ! self::is_supported_wp_version() ) {
+			return false;
+		}
+
+	}
+
+	/**
+	 * Checks the current PHP version against the required version.
+	 *
+	 * @return mixed
+	 */
+	protected static function is_supported_php_version() {
+
+		return version_compare( phpversion(), self::BWP_MIN_PHP_VERSION, '>=' );
+	}
+
+	/**
+	 * Checks the current WordPRess version against the required version.
+	 * 
+	 * @return mixed
+	 */
+	protected function is_supported_wp_version() {
+
+		global $wp_version;
+
+		return version_compare( $wp_version, self::BWP_MIN_WP_VERSION, '>=' );
+	}
+
+	/**
+	 * Displays a user friendly message in the WordPress admin.
+	 */
+	public static function display_admin_notices() {
+
+		echo '<div class="error"><p>' . self::get_notice_message() . '</p></div>';
+
+	}
+
+	/**
+	 * Returns a localized user friendly error message.
+	 *
+	 * @return string
+	 */
+	public static function get_notice_message() {
+
+		return sprintf(
+			__( 'BackUpWordPress requires PHP version %1$s or later and WordPress version %2$s or later to run. It has not been activated. %3$s%4$s%5$sLearn more%6$s', 'backupwordpress' ),
+			self::BWP_MIN_PHP_VERSION,
+			self::BWP_MIN_WP_VERSION,
+			'<a href="',
+			'https://bwp.hmn.md/unsupported-php-version-error/',
+			'">',
+			'</a>'
+		);
+	}
+
+}
