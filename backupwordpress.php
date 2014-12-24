2f1a3a57fd8efdd6cc3656dbbcc1c149468d6dea
paul de wouters <paul@hmn.md>
2014-12-24 08:17:35 -0300

    Re-uglify Intercom js

--- backupwordpress.php
+++ backupwordpress.php
@@ -150,7 +150,6 @@ class BackUpWordPress_Plugin {
 	 */
 	protected function includes() {
 
-
 		require_once( HMBKP_PLUGIN_PATH . 'classes/class-notices.php' );
 
 		// Load the admin menu
@@ -433,39 +432,7 @@ class BackUpWordPress_Plugin {
 		<script id="IntercomSettingsScriptTag">
 			window.intercomSettings = <?php echo json_encode( $info ); ?>;
 		</script>
-		<script>(function () {
-				var w = window;
-				var ic = w.Intercom;
-				if (typeof ic === "function") {
-					ic('reattach_activator');
-					ic('update', intercomSettings);
-				} else {
-					var d = document;
-					var i = function () {
-						i.c(arguments)
-					};
-					i.q = [];
-					i.c = function (args) {
-						i.q.push(args)
-					};
-					w.Intercom = i;
-					function l() {
-						var s = d.createElement('script');
-						s.type = 'text/javascript';
-						s.async = true;
-						s.src = 'https://static.intercomcdn.com/intercom.v1.js';
-						var x = d.getElementsByTagName('script')[0];
-						x.parentNode.insertBefore(s, x);
-					}
-
-					if (w.attachEvent) {
-						w.attachEvent('onload', l);
-					} else {
-						w.addEventListener('load', l, false);
-					}
-				}
-				;
-			})()</script>
+		<script>!function(){function e(){var a=c.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://static.intercomcdn.com/intercom.v1.js";var b=c.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)}var a=window,b=a.Intercom;if("function"==typeof b)b("reattach_activator"),b("update",intercomSettings);else{var c=document,d=function(){d.c(arguments)};d.q=[],d.c=function(a){d.q.push(a)},a.Intercom=d,a.attachEvent?a.attachEvent("onload",e):a.addEventListener("load",e,!1)}}();</script>
 
 	<?php }
 
