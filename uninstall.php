2ff2b3c277e4088980ec8b2b78082bbf12d29b53
paul de wouters <paul@hmn.md>
2015-02-01 18:20:33 +0000

    Do not delete backups on uninstall

--- uninstall.php
+++ uninstall.php
@@ -15,18 +15,13 @@ $schedules = $wpdb->get_col( $wpdb->prepare( "SELECT option_name FROM $wpdb->opt
 
 array_map( 'delete_option', $schedules );
 
-// Remove the backups directory
-require_once( plugin_dir_path( __FILE__ ) . 'classes/class-backup.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'classes/class-hmbkp-path.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'functions/core.php' );
-hmbkp_rmdirtree( hmbkp_path() );
-
 // Remove all the options
-foreach ( array( 'hmbkp_enable_support', 'hmbkp_plugin_version', 'hmbkp_path', 'hmbkp_default_path', 'hmbkp_upsell' ) as $option ) {
-	delete_option( $option );
-}
+
+$options = array( 'hmbkp_enable_support', 'hmbkp_plugin_version', 'hmbkp_path', 'hmbkp_default_path', 'hmbkp_upsell' );
+
+array_map( 'delete_option', $options );
 
 // Delete all transients
-foreach ( array( 'hmbkp_plugin_data', 'hmbkp_directory_filesizes', 'hmbkp_directory_filesize_running' ) as $transient ) {
-	delete_transient( $transient );
-}
+$transients = array( 'hmbkp_plugin_data', 'hmbkp_directory_filesizes', 'hmbkp_directory_filesize_running' );
+
+array_map( 'delete_transient', $transients );
