84b7d0ec856991b4373be4b609d44d03d2ac3c9f
paul de wouters <paul@hmn.md>
2015-02-25 17:10:29 +0000

    Add condtion for Cron and Ajax

--- backupwordpress.php
+++ backupwordpress.php
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 namespace HM\BackUpWordPress;
 
-if ( ! is_admin() ) {
+if ( ! is_admin() || ( defined( 'DOING_CRON' ) && DOING_CRON ) || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
 	return;
 }
 
