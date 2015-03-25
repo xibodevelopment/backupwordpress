bd2b49dbbb561edf0c1bfd91640301c9eb90e225
paul de wouters <paul@hmn.md>
2015-03-25 21:29:44 +0000

    Bump version

--- classes/class-plugin.php
+++ classes/class-plugin.php
@@ -11,8 +11,7 @@ register_deactivation_hook( __FILE__, array( 'HM\BackUpWordPress\Setup', 'deacti
  * Class Plugin
  */
 final class Plugin {
-
-	const PLUGIN_VERSION = '3.2.1';
+	const PLUGIN_VERSION = '3.2.2';
 
 	/**
 	 * @var Plugin The singleton instance.
