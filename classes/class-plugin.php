d98e11e1d1aab1a4f554012dd778add83bac59a9
Tom Willmot <tom@humanmade.co.uk>
2016-01-21 12:17:59 +0000

    bump for 3.4.1

--- classes/class-plugin.php
+++ classes/class-plugin.php
@@ -6,7 +6,7 @@ namespace HM\BackUpWordPress;
  * Class Plugin
  */
 final class Plugin {
-	const PLUGIN_VERSION = '3.4.0';
+	const PLUGIN_VERSION = '3.4.1';
 
 	/**
 	 * @var Plugin The singleton instance.
