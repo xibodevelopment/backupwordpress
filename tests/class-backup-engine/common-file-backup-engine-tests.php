cdbc1cfdb3617c5b09fa32325e73425d6d4e68e2
Tom Willmot <tom@humanmade.co.uk>
2016-02-28 20:05:49 +0000

    Unit tests for verify backup
    Ensure verify_backup returns false if there are backup errors

--- tests/class-backup-engine/common-file-backup-engine-tests.php
+++ tests/class-backup-engine/common-file-backup-engine-tests.php
@@ -272,4 +272,13 @@ abstract class Common_File_Backup_Engine_Tests extends \HM_Backup_UnitTestCase {
 
 	}
 
+	public function test_backup_with_errors() {
+
+		$this->assertTrue( $this->backup->backup() );
+		$this->assertFileExists( $this->backup->get_backup_filepath() );
+
+		$this->backup->error( get_class( $this->backup ), 'foobar' );
+		$this->assertFalse( $this->backup->verify_backup() );
+
+	}
 }
