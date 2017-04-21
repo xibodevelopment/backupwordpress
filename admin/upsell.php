d6ee584b9970f22acfe17aaa74b27491c5269e20
Katrina Moody <katrina@katsmedia.com>
2017-04-21 14:39:18 -0400

    Changing out upsell link (at bottom of admin page) to reflect link to site homepage for conversion funnel rather than link to in-admin extension page.

--- admin/upsell.php
+++ admin/upsell.php
@@ -7,7 +7,7 @@
 /** translators:  the 1st placeholder is the first part of the anchor tag with the link to the extensions admin page and the second is the closing anchor tag */
 printf(
 	__( 'Store your backups securely in the Cloud with %1$sour extensions%2$s', 'backupwordpress' ),
-    '<a href="https://bwp.hmn.md/" target="_blank">',
+    '<a href="<?php echo esc_url( https://bwp.hmn.md/ ); ?>" target="_blank">',
     '</a>'
 );
 ?>
